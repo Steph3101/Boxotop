052255b16579a686aea9ecaca6998d287db58815
Stéphane AZZOPARDI <contact@stephane-azzopardi.fr>
2018-10-04 12:03:35 +0200

    Add movie detail view controller

---
+++ Boxotop/Boxotop/View/MovieViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieViewController.swift
+//  Boxotop
+//
+//  Created by Stéphane AZZOPARDI on 04/10/2018.
+//  Copyright © 2018 Stéphane Azzopardi. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
