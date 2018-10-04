c3b41a7acaa120067669cf91f47e33fdbc1a64d5
Stéphane AZZOPARDI <contact@stephane-azzopardi.fr>
2018-10-04 14:21:54 +0200

    Adds movies tableview

--- Boxotop/Boxotop/View/MoviesViewController.swift
+++ Boxotop/Boxotop/View/MoviesViewController.swift
@@ -10,7 +10,39 @@ import UIKit
 
 class MoviesViewController: UIViewController {
 
+    @IBOutlet weak var moviesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        moviesTableView.dataSource      = self
+        moviesTableView.delegate        = self
+        moviesTableView.tableFooterView = UIView()
+    }
+}
+
+extension MoviesViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension MoviesViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Movie selected")
+    }
+
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
